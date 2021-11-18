8c7129d7bbab4da655218272f79204c8182b229a
flashy86 <annecharlotte.lucas2021@campus-eni.fr>
2021-11-18 11:51:20 +0100

    ajout package intl + extrapolation des Strings

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import 'transaction.dart';
 
@@ -71,12 +74,16 @@ class MyHomePage extends StatelessWidget {
                         border: Border.all(color: Colors.purple, width: 2)),
                     padding: EdgeInsets.all(10),
                     child: Text(
-                      tx.amount.toString(),
+                      '\$${tx.amount}',
                       style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 20,
                           color: Colors.purple),
                     ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                    height: 15,
                   ),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
@@ -87,7 +94,7 @@ class MyHomePage extends StatelessWidget {
                             fontSize: 16, fontWeight: FontWeight.bold),
                       ),
                       Text(
-                        tx.date.toString(),
+                        DateFormat().add_yMMMd().format(tx.date),
                         style: TextStyle(color: Colors.grey),
                       )
                     ],
